2d1d194b08ee3a2e05a14465d407039fef815896
kaho <57692753+tanakaho@users.noreply.github.com>
2022-01-16 21:35:15 +0900

    a

--- server.js
+++ server.js
@@ -15,6 +15,54 @@ app.get("/", (req, res) => {
 
 app.post("/webhook", function(req, res) {
     res.send("HTTP POST request sent to the webhook URL!")
+    // ユーザーがボットにメッセージを送った場合、返信メッセージを送る
+    if (req.body.events[0].type === "message") {
+    // 文字列化したメッセージデータ
+    const dataString = JSON.stringify({
+        replyToken: req.body.events[0].replyToken,
+        messages: [
+        {
+            "type": "text",
+            "text": "Hello, user"
+        },
+        {
+            "type": "text",
+            "text": "May I help you?"
+        }
+        ]
+    })
+
+    // リクエストヘッダー
+    const headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + TOKEN
+    }
+
+    // リクエストに渡すオプション
+    const webhookOptions = {
+        "hostname": "api.line.me",
+        "path": "/v2/bot/message/reply",
+        "method": "POST",
+        "headers": headers,
+        "body": dataString
+    }
+
+    // リクエストの定義
+    const request = https.request(webhookOptions, (res) => {
+        res.on("data", (d) => {
+        process.stdout.write(d)
+        })
+    })
+
+    // エラーをハンドル
+    request.on("error", (err) => {
+        console.error(err)
+    })
+
+    // データを送信
+    request.write(dataString)
+    request.end()
+    }
 })
 
 app.listen(PORT, () => {
