f3bc35622dae2ba81c3d625ef77ed90bdc1393e3
kaho <57692753+tanakaho@users.noreply.github.com>
2022-01-23 22:35:52 +0900

    a

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const express = require("express")
 const line = require('@line/bot-sdk');
 
 // 署名検証
-const crypto = require('crypto')
+const crypto = require('crypto');
+const { resolve } = require('path');
+const { rejects } = require('assert');
+const { Fdkaac } = require('node-fdkaac');
 function validateSignature(signature, body) {
     const LINE_CHANNEL_SECRET = process.env.CHANNEL_SECLET
     return signature == crypto.createHmac('sha256', LINE_CHANNEL_SECRET).update(Buffer.from(JSON.stringify(body))).digest('base64')
@@ -79,44 +82,82 @@ app.post("/webhook", function(req, res) {
         break;
         
         case "audio":
-            var dataString = JSON.stringify({
-                replyToken:req.body.events[0].replyToken,
-                messages: [
-                    {
-                        "type": "text",
-                        "text": "Hello, user"
-                    },
-                    {
-                        "type": "text",
-                        "text": "this is audio"
-                    }
-                ]
-            })
-            var headers = {
-                "Content-Type": "application/json",
-                "Authorization": "Bearer " + TOKEN
+            var audioData = fetchAudioMessage(event.message.id
+                ).then(function (audioData) {
+                    convert(audioData)
+                        .then(function (audioBytes) {
+                            return describe(audioBytes);
+                        }).then(function (text) {
+                            lineClient.replyMessage(event.replyToken, {
+                                type: 'text',
+                                text:text
+                            });
+                        });
+                });
+            function fetchAudioMessage(messageId){
+                return new Promise((resolve, reject) => {
+                    lineClient.getMessageContent(messageId).then((stream) => {
+                        const content = [];
+                        stream
+                            .on('data', (chunk) => {
+                                content.push(new Buffer(chunk));
+                            })
+                            .on('error', (err) => {
+                                reject(err);
+                            })
+                            .on('end', function() {
+                                resolve(Buffer.concat(content));
+                            });
+                    });
+                });
             }
-    
-            var webhookOptions = {
-                "hostname": "api.line.me",
-                "path": "/v2/bot/message/reply",
-                "method": "POST",
-                "headers": headers,
-                "body": dataString
+            function convert(audioData) {
+                return new Promise((resolve, reject) => {
+                    const decoder = new Fdkaac({
+                        "output": "buffer",
+                        "bitrate": 192
+                    }).setBuffer(audioData);
+
+                    decoder.decode()
+                        .then(() => {
+                            //Encoding finished
+                            const buffer = decoder.getBuffer();
+                            const audioBytes = buffer.toString('base64');
+                            resolve(audioBytes);
+                        })
+                        .catch((error) => {
+                            console.log("decode error:", error);
+                        });
+                });
             }
-    
-            var request = https.request(webhookOptions, (res) => {
-                res.on("data", (d) => {
-                    process.stdout.write(d)
-                })
-            })
-    
-            request.on("error", (err) => {
-                console.error(err)
-            })
-    
-            request.write(dataString)
-            request.end()
+            const speech = require('@google-cloud/speech');
+            const speechClient = new speech.speechClient();
+
+            function describe(audioBytes) {
+                return new Promise((resolve, reject) => {
+                    const audio = {
+                        content: audioBytes,
+                    };
+                    const config = {
+                        encoding: 'LINEAR16',
+                        sampleRateHertz: 16000,
+                        languageCode: 'ja-JP',
+                    };
+
+                    speechClient
+                        .recognize(Request)
+                        .then(data => {
+                            const response = data[0];
+                            const transcription = response.results
+                            .map(result => result.alternatives[0].transcript)
+                            .join('\n');
+                            resolve(transcription);
+                        })
+                        .catch(err => {
+                            console.error('ERROR:',err);
+                        });
+                });
+            }
             break;
     }
 })
