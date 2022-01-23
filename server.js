14601cc46a376cc4dfc149a7406797b0da618519
kaho <57692753+tanakaho@users.noreply.github.com>
2022-01-23 16:52:51 +0900

    a

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
-};
+// if (process.env.NODE_ENV !== 'production') {
+//     require('dotenv').config()
+// };
 
 const line = require('@line/bot-sdk');
 const express = require('express');
