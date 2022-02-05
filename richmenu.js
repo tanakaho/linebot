6d2f9c0f3a3e2f385d60880994e6f05cab6444f0
kaho <57692753+tanakaho@users.noreply.github.com>
2022-02-05 14:14:23 +0900

    a

--- richmenu.js
+++ richmenu.js
@@ -1,12 +1,49 @@
 const line = require('@line/bot-sdk');
 
-const client = new line.Client({
-    channelAccessToken:process.env.CHANNEL_ACCSESS_TOKEN
-});
+const TOKEN = process.env.CHANNEL_ACCSESS_TOKEN
 
-const richmenu = {
-};
+exports.make_richmenu = function(){
+    const richmenu = {
+        "size": {
+            "width": 2500,
+            "height": 1686
+        },
+        "selected": false,
+        "name": "stopwatch",
+        "chatBarText": "Tap to open",
+        "areas": [
+            {
+                "bounds": {
+                    "x": 0,
+                    "y": 0,
+                    "width": 2500,
+                    "height": 1686
+                },
+                "action": {
+                    "type":"message",
+                }
+            }
+        ]
+    };
 
-client.createRichMenu(richmenu)
-    .then((richmenuId) => 
-    console.log(richmenuId))
+    var headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + TOKEN
+    }
+    var webhookOptions = {
+        "hostname": "api.line.me",
+        "path": `/v2/bot/richmenu`,
+        "method": "POST",
+        "headers": headers,
+    }
+
+    var request = https.request(webhookOptions, (res) => {
+        res.on("data", (d) => {
+            process.stdout.write(d);
+        })
+    })
+    request.on("error", (err) => {
+        process.stdout.write(`richmanuErrer:${err}`)
+    })
+    request.end()
+}
